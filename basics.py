5a30596ecd673a14e1f143a4c2c3122102ec8b8a
Sander van der Maar <sandervandermaar@gmail.com>
2021-10-16 09:55:42 -0300

    Introduces Text widget

--- basics.py
+++ basics.py
@@ -3,7 +3,10 @@ import tkinter as tk
 def OnButtonClick():
     #print(entry.get())
     #entry.delete(0,tk.END)
-    entry.insert(0, "Python")
+    #entry.insert(0, "Python")
+    #print(text_box.get("1.0", tk.END))
+    #text_box.delete("1.0", tk.END)
+    text_box.insert(tk.END, "Put me at the end!")
 
 window = tk.Tk()
 
@@ -17,9 +20,10 @@ greeting.pack()
 button = tk.Button(text="Click me!", width=25, height=5, bg="blue", fg="yellow", command=OnButtonClick)
 button.pack()
 
-entry = tk.Entry(fg="yellow", bg="blue", width=50)
-entry.pack()
+#entry = tk.Entry(fg="yellow", bg="blue", width=50)
+#entry.pack()
+
+text_box = tk.Text()
+text_box.pack()
 
 window.mainloop()
-
-
