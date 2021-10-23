e80157bfd3beb735341445b52a50f11bca4d9d0a
Sander van der Maar <sandervandermaar@gmail.com>
2021-10-23 09:42:19 -0300

    Shows expanding to fill whole window

--- pack1.py
+++ pack1.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 window = tk.Tk()
 
-frame1 = tk.Frame(height=100, bg="red")
-frame1.pack(fill=tk.X)
+frame1 = tk.Frame(width=200, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-frame2 = tk.Frame(height=50, bg="yellow")
-frame2.pack(fill=tk.X)
+frame2 = tk.Frame(width=100, bg="yellow")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
-frame3 = tk.Frame(height=25, bg="blue")
-frame3.pack(fill=tk.X)
+frame3 = tk.Frame(width=50, bg="blue")
+frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
 
 window.mainloop()
-
