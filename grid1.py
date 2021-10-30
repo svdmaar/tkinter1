58f73e61978e760b041289ba1c0f3ec01759beeb
Sander van der Maar <sandervandermaar@gmail.com>
2021-10-30 09:16:50 -0300

    Makes frames move with window resize

--- grid1.py
+++ grid1.py
@@ -1,8 +1,17 @@
 import tkinter as tk
 window = tk.Tk()
 
-for i in range(3):
-    for j in range(4):
+rowCount = 3
+columnCount = 4
+
+for i in range(rowCount):
+    window.rowconfigure(i, weight=1, minsize=70)
+
+for j in range(columnCount):
+    window.columnconfigure(j, weight=1, minsize=100)
+
+for i in range(rowCount):
+    for j in range(columnCount):
         frame = tk.Frame(relief=tk.RAISED, borderwidth=1)
         frame.grid(row=i, column=j, padx=5, pady=5)
         label = tk.Label(master=frame, text="Row " + str(i) + "\nColumn " + str(j))
